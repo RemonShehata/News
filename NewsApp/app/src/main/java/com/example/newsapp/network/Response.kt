83765b7f530a0b28cd07fc11926d7c2afdfc95aa
Remon Shehata <remongerges94@gmail.com>
2022-03-18 23:43:51 +0200

    add response data classes

---
+++ NewsApp/app/src/main/java/com/example/newsapp/network/Response.kt
@@ -0,0 +1,34 @@
+package com.example.newsapp.network
+
+sealed class Response {
+    object Loading: Response()
+    data class Success(val data: News) : Response()
+    data class Failure(val reason: FailureReason)
+}
+
+data class News(
+    val status: String,
+    val totalResults: Int,
+    val articles: List<Article>,
+    val code: String?,
+    val message: String?
+)
+
+data class Article(
+    val source: Source,
+    val author: String?,
+    val title: String,
+    val url: String,
+    val urlToImage: String,
+    val publishedAt: String,
+    val content: String
+)
+
+data class Source(
+    val id: String?,
+    val name: String
+)
+
+sealed class FailureReason {
+    object NoInternet : FailureReason()
+}
