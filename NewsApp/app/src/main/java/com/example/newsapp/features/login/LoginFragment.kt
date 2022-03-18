db757950d05e6e48b281fb7f32ee5225158679f2
Ibrahim Abousalem <hima.abousalem@gmail.com>
2022-03-18 21:16:22 +0200

    app auth skelton

---
+++ NewsApp/app/src/main/java/com/example/newsapp/features/login/LoginFragment.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.features.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
