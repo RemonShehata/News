478344d8ec7db1b82c6c5dfb4add2434140a6d07
Remon Shehata <remongerges94@gmail.com>
2022-03-19 12:38:20 +0200

    create initial layout for news list fragment

--- NewsApp/app/src/main/java/com/example/newsapp/features/news/NewsListFragment.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/features/news/NewsListFragment.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.data.network.News
+import com.example.newsapp.databinding.FragmentNewsListBinding
 
 class NewsListFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewsListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_list, container, false)
+        binding = FragmentNewsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val newsListAdapter = NewsListAdapter()
+        binding.newsListRecycler.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = newsListAdapter
+        }
     }
 }
