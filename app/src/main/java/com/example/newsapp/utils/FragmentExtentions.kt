7e5c81190e4edda07b75e2dd54cc3e7901dac177
Remon Shehata <remongerges94@gmail.com>
2022-03-20 00:04:54 +0200

    add extension for the snack bar

---
+++ app/src/main/java/com/example/newsapp/utils/FragmentExtentions.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+fun Fragment.showMessage(message: String) {
+    Snackbar.make(this.requireView(), message, Snackbar.LENGTH_SHORT).show()
+}
