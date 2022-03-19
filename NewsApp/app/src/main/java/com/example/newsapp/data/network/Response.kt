672d1a2db28bfcdeb4f84ea210764c7d388b121f
Remon Shehata <remongerges94@gmail.com>
2022-03-19 16:06:31 +0200

    make article non nullable in api response

--- NewsApp/app/src/main/java/com/example/newsapp/data/network/Response.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/data/network/Response.kt
@@ -9,7 +9,7 @@ sealed class Response {
 data class News(
     val status: String,
     val totalResults: Int,
-    val articles: List<Article>?,
+    val articles: List<Article>,
 )
 
 data class Article(
