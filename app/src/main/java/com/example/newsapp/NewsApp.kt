31b5ede859823751d4fb63851d435206197b3ff9
Remon Shehata <remongerges94@gmail.com>
2022-03-27 23:35:18 +0200

    fix head title in pr template make the app compile again

--- app/src/main/java/com/example/newsapp/NewsApp.kt
+++ app/src/main/java/com/example/newsapp/NewsApp.kt
@@ -8,7 +8,6 @@ class NewsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        NewsManager.initialize(context = applicationContext) tttt
-                add new changes
+        NewsManager.initialize(context = applicationContext)
     }
 }
