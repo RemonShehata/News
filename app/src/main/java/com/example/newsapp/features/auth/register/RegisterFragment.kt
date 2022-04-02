d300ff620c7c9e1e4b70b13525abcd60bd798443
Remon Shehata <remongerges94@gmail.com>
2022-04-02 18:58:32 +0200

    use the new view model in register fragment

--- app/src/main/java/com/example/newsapp/features/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/newsapp/features/auth/register/RegisterFragment.kt
@@ -4,21 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.example.newsapp.R
-import com.example.newsapp.data.entities.User
+import com.example.newsapp.data.entities.UserDto
 import com.example.newsapp.databinding.FragmentRegisterBinding
 import com.example.newsapp.di.NewsManager
-import com.example.newsapp.features.auth.AuthViewModel
 import com.example.newsapp.features.auth.MyViewModelFactory
+import com.example.newsapp.utils.getTrimmedText
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
 
-    private val viewModel: AuthViewModel by viewModels {
+    private val viewModel: RegisterViewModel by viewModels {
         MyViewModelFactory(NewsManager.userRepo)
     }
 
@@ -27,59 +24,48 @@ class RegisterFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        binding = FragmentRegisterBinding.inflate(layoutInflater).apply {
+            registerBtn.setOnClickListener {
+                doRegister()
+            }
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.registerBtn.setOnClickListener {
-            doRegister()
-        }
 
-        viewModel.registrationResultLiveData.observe(viewLifecycleOwner) {
-            if (it) {
-                requireActivity().finish()
-                findNavController().navigate(R.id.newsActivity)
+        viewModel.registrationResultLiveData.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is RegisterResult.InvalidData -> {
+                    when (result.error) {
+                        ErrorType.EmptyEmail -> TODO()
+                        ErrorType.EmptyName -> TODO()
+                        ErrorType.EmptyPassword -> TODO()
+                        ErrorType.EmptyPhoneNumber -> TODO()
+                        ErrorType.InvalidEmailFormat -> TODO()
+                        ErrorType.InvalidPasswordFormat -> TODO()
+                        ErrorType.InvalidPhoneNumberFormat -> TODO()
+                    }
+                }
+                RegisterResult.RegisterError -> {
+                    TODO()
+                }
+                RegisterResult.RegisterSuccessful -> {
+                    TODO()
+                }
             }
         }
     }
 
     private fun doRegister() {
-        val email = binding.emailET.text.toString().trim()
-        val password = binding.passwordET.text.toString().trim()
-        val userName = binding.userNameET.text.toString().trim()
-        val phoneNumber = binding.phoneNumberET.text.toString().trim()
-        if (isValidData(email, password, userName, phoneNumber)) {
-            Toast.makeText(requireContext(), "please fill all the fields!", Toast.LENGTH_SHORT)
-                .show()
-        } else if (!viewModel.isValidEmail(email)) {
-            Toast.makeText(requireContext(), "please Enter a valid email!", Toast.LENGTH_SHORT)
-                .show()
-        } else if (!viewModel.isValidPhoneNumber(phoneNumber)) {
-            Toast.makeText(
-                requireContext(),
-                "please Enter a valid phone number!",
-                Toast.LENGTH_SHORT
-            )
-                .show()
-        } else {
-            val user = User(
-                email = email,
-                name = userName,
-                phoneNumber = phoneNumber,
-                password = password
-            )
-            viewModel.register(user)
-        }
-    }
+        val email = binding.emailET.getTrimmedText()
+        val password = binding.passwordET.getTrimmedText()
+        val userName = binding.userNameET.getTrimmedText()
+        val phoneNumber = binding.phoneNumberET.getTrimmedText()
 
-    private fun isValidData(
-        email: String,
-        password: String,
-        userName: String,
-        phoneNumber: String
-    ): Boolean {
-        return email.isEmpty() || password.isEmpty() || userName.isEmpty() || phoneNumber.isEmpty()
+        val userDto =
+            UserDto(email = email, name = userName, password = password, phoneNumber = phoneNumber)
+        viewModel.register(userDto)
     }
 }
