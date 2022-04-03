0719db1da3fdea527cb701aeb3b05881daa828ea
Remon Shehata <remongerges94@gmail.com>
2022-04-03 21:55:23 +0200

    fix table name

--- app/src/main/java/com/example/newsapp/data/entities/UserEntity.kt
+++ app/src/main/java/com/example/newsapp/data/entities/UserEntity.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.data.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "user")
 data class UserEntity(
     @PrimaryKey(autoGenerate = false)
     val email: String,
