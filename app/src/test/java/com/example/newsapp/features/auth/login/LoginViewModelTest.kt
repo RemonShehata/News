da5a388d78300b0d6057aee4895ef0a363c7b562
Remon Shehata <remongerges94@gmail.com>
2022-04-16 23:36:46 +0200

    add tests for login scenarios

--- app/src/test/java/com/example/newsapp/features/auth/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/newsapp/features/auth/login/LoginViewModelTest.kt
@@ -1,3 +1,5 @@
+@file: Suppress("NoWildcardImports")
+
 package com.example.newsapp.features.auth.login
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
@@ -5,16 +7,18 @@ import com.example.newsapp.data.repos.UserRepository
 import com.example.newsapp.util.TestCoroutineRule
 import com.example.newsapp.util.getOrAwaitValue
 import com.example.newsapp.utils.isValidEmailFormat
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockkStatic
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import kotlin.test.assertIs
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class LoginViewModelTest {
     @get:Rule
     val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
@@ -33,6 +37,11 @@ class LoginViewModelTest {
         loginViewModel = LoginViewModel(userRepo)
         // https://stackoverflow.com/questions/44382540/mocking-extension-function-in-kotlin
         mockkStatic("com.example.newsapp.utils.ExtentionsUtilKt")
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
     }
 
     @Test
@@ -83,4 +92,41 @@ class LoginViewModelTest {
         assertIs<LoginResult.InvalidData>(result)
         assertIs<ErrorType.EmptyPassword>(result.error)
     }
+
+    @Test
+    fun `given valid email and password, when login is called, Success is returned`() = runTest {
+        // GIVEN
+        val email = "test@test.com"
+        val password = "123456789"
+        every { any<String>().isValidEmailFormat() } returns true
+        coEvery { userRepo.login(email, password) } returns true
+
+        // WHEN
+        loginViewModel.login(email, password)
+
+        // THEN
+        val result = loginViewModel.loginResultLiveData.getOrAwaitValue()
+
+        coVerify { userRepo.login(email, password) }
+        assertIs<LoginResult.Success>(result)
+    }
+
+    @Test
+    fun `given valid email and password, when login is called, WrongCredentials is returned`() =
+        runTest {
+            // GIVEN
+            val email = "test@test.com"
+            val password = "123456789"
+            every { any<String>().isValidEmailFormat() } returns true
+            coEvery { userRepo.login(email, password) } returns false
+
+            // WHEN
+            loginViewModel.login(email, password)
+
+            // THEN
+            val result = loginViewModel.loginResultLiveData.getOrAwaitValue()
+
+            coVerify { userRepo.login(email, password) }
+            assertIs<LoginResult.WrongCredentials>(result)
+        }
 }
